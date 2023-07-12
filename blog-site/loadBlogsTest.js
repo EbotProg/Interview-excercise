6a35d22078b1dbae7609657cd82d785bce4bc37f
ebotprog <achaleebotoma2002@gmail.com>
2023-07-12 08:56:03 +0100

    added new functionality

--- blog-site/loadBlogsTest.js
+++ blog-site/loadBlogsTest.js
@@ -5,8 +5,10 @@ export function loadIdentities() {
       isLoading: false,
       allGists: [],
       gistsLength: null,
+      gistIdsArr: [],
       allCategories: [], 
       allAuthors: [],
+      authorIdsArr: [],
       init() {
         try {
           this.isLoading = true
@@ -15,21 +17,44 @@ export function loadIdentities() {
           .then(data => {
             this.allGists = data.data;
             this.gistsLength = this.allGists.length;
+            this.allGists.forEach((gist, index, arr)=>{
+            this.gistIdsArr.push(gist.id);
+            })
             console.log('Gists: ', this.allGists, this.gistsLength);
-            
+            console.log('gist ids', this.gistIdsArr)
+
+           //
+           fetch(`https://gistora.com/api/authors`)
+           .then(res => res.json())
+           .then(data => {
+             this.allAuthors = data.data;
+             this.allAuthors.forEach((author, index, arr)=>{
+               this.authorIdsArr.push(author.id);
+               })
+             console.log('authors: ',this.allAuthors);
+             console.log('author ids', this.authorIdsArr);
+
+
+
+             //
+             fetch(`https://gistora.com/api/categories`)
+             .then(res => res.json())
+             .then(data => {
+               this.allCategories = data.data;
+               console.log('categories: ', this.allCategories);
+
+               console.log('id test',this.authorIdsArr, this.authorIdsArr.find((authorId, index, arr)=>2===authorId))
+
+             })
+              //
+
+           })
+           //
+
+
           })
-        fetch(`https://gistora.com/api/authors`)
-          .then(res => res.json())
-          .then(data => {
-            this.allAuthors = data.data;
-            console.log('authors: ',this.allAuthors);
-          })
-        fetch(`https://gistora.com/api/categories`)
-          .then(res => res.json())
-          .then(data => {
-            this.allCategories = data.data;
-            console.log('categories: ', this.allCategories);
-          })
+       
+        
         this.isLoading = false;
         } catch (error) {
           console.error('This is error', error);
